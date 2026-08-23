983270f90d7d1347bd416d11d390c36f99dbeb5d
rems80000 <remy.portaux@gmail.com>
2026-08-23 17:09:16 +0200

    Préparer l'application Android native (#17)
    Ajoute le projet TWA et un workflow qui produit un APK Android de test sans exposer la clé de signature.

---
+++ android-twa/app/src/main/java/fr/remy/lifehub/DelegationService.java
@@ -0,0 +1,14 @@
+package fr.remy.lifehub;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
